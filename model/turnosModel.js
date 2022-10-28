2819147428642a396e7c3b649c4a414904ab1bc2
PhDNoe <nigth.crawler25@gmail.com>
2022-10-28 16:28:40 -0300

    first Noe commit

---
+++ model/turnosModel.js
@@ -0,0 +1,58 @@
+const turnos = require('../data/turnos')
+const filemanagement = require('../utils')
+const fs = require('fs')
+const { Sinturno } = require('./Errors/sinTurno')
+const { TurnoOcupado } = require('./Errors/turnoOcupado')
+function findAll() {
+    return new Promise((resolve, reject) => {
+        resolve(turnos)
+    })
+}
+
+function create(turno) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/turnos.json', function (err, data) {
+
+            if (err) throw err;
+
+            const mapa = (JSON.parse(data)).map(function (element) {
+                return element;
+            });
+            console.log("create-turno-mapa --> ", mapa);
+            console.log("Turno que intento cargar --> ", turno);
+            let turnoFecha = mapa.filter(turnoB => {
+                return turnoB["fecha"] == turno.fecha && turnoB["branchId"] == turno.branchId;
+            }).map(turnoB => {
+                return turnoB;
+            });
+
+            console.log("turno-fecha --> ", turnoFecha)
+            if (turnoFecha.length == 0) {
+                reject(new Sinturno())
+            } else {
+
+                console.log(turnoFecha);
+
+                if (turnoFecha[0]['userid'] == null) {
+                    turnoFecha[0]['userid'] = turno['userid'];
+                    turnoFecha[0]['email'] = turno['email'];
+                    const newTurno = turno
+                    //turnos.push(newTurno)
+
+                    filemanagement.writeDataToFile('./data/turnos.json', mapa)
+                    resolve(newTurno)
+                } else {
+                    reject(new TurnoOcupado());
+                }
+            }
+
+        });
+
+
+    })
+}
+
+module.exports = {
+    findAll,
+    create
+}
---
+++ server.js
@@ -0,0 +1,106 @@
+const http = require('http')
+const Turnos = require('./model/turnosModel')
+const { getPostData } = require('./utils')
+const { hashCode } =require('./utils')
+const fs = require('fs')
+
+const headers = {
+    'Access-Control-Allow-Origin': '*', 
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Content-Type': 'application/json'
+}
+
+const header1 = {
+    'Access-Control-Allow-Origin': '*', 
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Content-Type': 'text/html'
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url==="/" && req.method==='GET') {
+        fs.readFile('index.html',function (err, data){
+            res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+            res.write(data);
+            res.end();
+        });}
+    if (req.url==="/" && req.method==='POST') {
+        createTurno(req, res)
+    }
+    else if (req.url === '/api/turnos' && req.method === 'GET') {
+        getTurnos(req, res);
+    }
+    else if (req.url === '/api/turnos' && req.method === 'POST') {
+
+        createTurno(req, res)
+    }
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = server;
+
+
+// @desc    Gets all Turnos
+// @route   GET /api/turnos
+async function getTurnos(req, res) {
+    try {
+        const turnos = await Turnos.findAll()
+
+        const headers = {
+            'Access-Control-Allow-Origin': '*', 
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+            'Content-Type': 'application/json'
+        }
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(turnos))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// @desc    Create a Turno
+// @route   POST /api/turnos
+async function createTurno(req, res) {
+    try {
+
+        const body = await getPostData(req);
+
+        console.log(body);
+
+        const { fecha, email, branchId_s } = JSON.parse(body);
+
+      
+        userid=hashCode(email);
+        branchId = parseInt(branchId_s);
+
+
+
+        const turno = {
+            fecha,
+            userid,
+            email,
+            branchId
+        }
+
+        const newTurno = Turnos.create(turno).then( ()=>{
+            res.writeHead(201, headers);
+            return res.end(JSON.stringify(newTurno));
+
+        },(error)=>{
+            console.log(error.status);
+            res.writeHead(error.status,headers);
+            res.end(error.toString());
+        } )
+
+
+        
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
